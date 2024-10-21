86ebd8d368e2b41368962eb0f8a103377f65d3d2
HotoRas <hotoras03@gmail.com>
2024-10-21 19:13:10 +0900

    Fix (frontend): 새 계정 생성 버튼 복귀
    계정 생성이 수동이 아니게 됐으니..

--- packages/frontend/src/account.ts
+++ packages/frontend/src/account.ts
@@ -313,10 +313,10 @@ export async function openAccountMenu(opts: {
 			children: [{
 				text: i18n.ts.existingAccount,
 				action: () => { showSigninDialog(); },
-			}, /*{
+			}, {
 				text: i18n.ts.createAccount,
 				action: () => { createAccount(); },
-			}*/],
+			}],
 		}, {
 			type: 'link',
 			icon: 'ti ti-users',
